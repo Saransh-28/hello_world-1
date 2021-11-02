32dd568529289928ddee22f5f69d2408ba1f363a
saransh bhandari <saranshbhandari28@gmail.com>
2021-11-02 22:24:28 +0530

    commit

--- src/main.c
+++ src/main.c
@@ -1,17 +1,14 @@
 #include<stdio.h>
-#include <stdio.h>
-#include "..\include\myfile.h"
-
-int main(){
-
-    char name[] = "Abhinav";
-    sgs_clib_convert_to_upper(name);
-
-    printf("After convering to all Caps name is: %s",name);
-#include"../include/hello.h"
-int main(){
-    char a[]="VivekDawar";
-     sgs_clib_convert_to_lower(a);
-    printf("%s",a);
-
+#include"../include/2d_array.h"
+int main()
+{
+    double arr[3][3];
+    int i;
+    sgs_clib_2d_double_array(arr);
+    for(i=0;i<3;i++)
+    for(int j=0;j<3;j++){
+    {
+        printf("%lf ",arr[i])[j];
+    }}
+    return 0;
 }
